3109bb6174790a921e18e8e707785bb1c18857a7
778758944 <778758944@qq.com>
2016-02-18 17:00:59 +0800

    modify

--- html/upload2.php
+++ html/upload2.php
@@ -6,30 +6,31 @@
  * @version $Id$
  */
 $data=$_POST["data"];
-$filename=dirname(__FILE__);
-chdir('/www/sgh');
-$imgname="aaa".".png";
+echo "hello world";
+// $filename=dirname(__FILE__);
+// chdir('/www/sgh');
+// $imgname="aaa".".png";
 
-$img_path1='./html/images/aaa.txt';
-$img_path2='/html/images/'.$imgname;
+// $img_path1='./html/images/aaa.txt';
+// $img_path2='/html/images/'.$imgname;
 
 
-function convert_data($data){
-	$image=base64_decode($data);
-	save_to_file($image);
-}
-function save_to_file($image){
+// function convert_data($data){
+// 	$image=base64_decode($data);
+// 	save_to_file($image);
+// }
+// function save_to_file($image){
 
-	$fp=fopen($img_path1,"r");
-	$return_data=fread($fp,filesize($img_path1));
-	// echo $return_data;
-	fclose($fp);
-	echo $return_data;
-	// $arr=array('data'=>$return_data);
-	// exit(json_decode($arr));
-}
+// 	$fp=fopen($img_path1,"r");
+// 	$return_data=fread($fp,filesize($img_path1));
+// 	// echo $return_data;
+// 	fclose($fp);
+// 	echo $return_data;
+// 	// $arr=array('data'=>$return_data);
+// 	// exit(json_decode($arr));
+// }
 
-convert_data($data);
+// convert_data($data);
 
 // $arr=array("url"=>$img_path2);
 
