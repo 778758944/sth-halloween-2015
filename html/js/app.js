7eb4a8cc197e7397fb2f273fdb2646b949aa554d
778758944 <778758944@qq.com>
2016-02-18 10:03:23 +0800

    modify

--- html/js/app.js
+++ html/js/app.js
@@ -1234,7 +1234,7 @@ app.directive("anfile",function($location,$timeout){
 						    success: function (res) {
 						        var serverId = res.serverId;
 						        console.log(use);
-						        console.log("get IMAGE");
+						        alert("get IMAGE");
 						        $.post("http://sgh.xingwentao.top/html/upload.php",{
 						        	server_id:serverId,
 						        	openid:use
